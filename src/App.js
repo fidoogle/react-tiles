610de9c09c2cec806388c05c82d05728481375cf
Fidel Guajardo <figuajardo@saws.org>
2020-01-30 15:33:33 -0600

    add page titles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Search from './components/search'
 import Tiles from './components/tiles'
 import Nav from './components/nav'
-
+import PageTitles from './components/page-titles'
 
 function App() {
   
   return (
     <>
       <Nav/>
+      <PageTitles/>
       <Search/>
       <Tiles/>
     </>
